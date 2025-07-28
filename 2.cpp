74537198ab49060ca0ebfb18242345a7c9771ba1
Prathamesh-2004 <144206193+Prathamesh-2004@users.noreply.github.com>
2025-07-28 10:42:17 +0530

    2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,46 +1,66 @@
+// Implement the problem of moving all zeroes to the end of array. 
+// Statement: Given an array of random numbers, Push all the zeroes of a 
+// given array to the end of the array. 
+
+
 #include<iostream>
 #include<vector>
 using namespace std;
+void mergestep(int array[],int start,int mid,int end)
+{
+    vector<int>temp;
+    int i=start;
+    int j=mid+1;
+   while(i<=mid&&j<=end)
+   {
+    if(array[i]>=array[j])
+    {
+        temp.push_back(array[i]);
+        i++;
+    }
+    else{
+        temp.push_back(array[j]);
+        j++;
+    }
+    }
+    
+   while(i<=mid)
+   {
+    temp.push_back(array[i]);
+    i++;
+   }
+   while(j<=end)
+   {
+    temp.push_back(array[j]);
+    j++;
+
+   }
+
+   for(int i=0;i<temp.size();i++)
+   {
+    array[i+start]=temp[i];
+   }
+
+}
+void merge(int array[],int start,int end)
+{
+if(start<end)
+{
+    int mid=(end+start)/2;
+    merge(array,start,mid);
+    merge(array,mid+1,end);
+    mergestep(array,start,mid,end);
+
+}
+
+}
 int main()
 {
-    vector<int>nums={0,1,2,0,1,2,3,0,0};
-    int end=nums.size()-1,temp;
-
-    // for(int i=0;i<nums.size();i++)
-    // {
-    //     if(end<=i)
-    //     {
-    //         break;
-    //     }
-    //     while(nums[end]==0)
-    //     {
-    //         end--;
-    //     }
-    //     if(nums[i]==0)
-    //     {
-    //         temp=nums[i];
-    //         nums[i]=nums[end];
-    //         nums[end]=temp;
-    //     }
-    // }
-
-
-    vector<int>nums2;
-    for(int i=0;i<nums.size();i++)
+    int array[10]={1,3, 4 ,0, 5, 6};
+    merge(array,0,5);
+        for(int i=0;i<6;i++)
     {
-        if(nums[i]!=0)
-        {
-            nums2.push_back(nums[i]);
-        }
+        cout<<array[i]<<" ";
     }
-    for(int i=nums2.size()-1;i<nums.size();i++)
-    {
-        nums2.push_back(0);+
-    }
-    nums=nums2;
-    cout<<"final array : ";
-    for(int i=0;i<nums.size();i++)
-    {
-        cout<<nums[i]<<" ";
-    }
+
 }
